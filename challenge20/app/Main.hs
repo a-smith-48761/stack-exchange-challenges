57d96c22248b46322ca2e0ad1f87d7af0e0640a8
a-smith-48761 <a.smith.48761@gmail.com>
2026-07-01 12:43:03 +0000

    Finalize report format
    * Switch to storing the scenarios in files rather than stdin
    * Produce correct report format for all scenarios, including scenario name.

--- challenge20/app/Main.hs
+++ challenge20/app/Main.hs
@@ -12,6 +12,8 @@ import Control.Arrow               -- used for easy-to-read function composition
 import qualified Data.Map as Map   -- maps
 import Data.Maybe (catMaybes)      -- function for flattening lists of Maybes
 import Data.Ratio                  -- rational types, for calculations involving scoops and tubs
+import System.FilePath             -- filename manipulation
+import Control.Monad               -- for looping constructs in monad contexts
 
 -- use a specific type for support names and ice cream flavors to help detect errors
 type Name = Text.Text
@@ -44,8 +46,9 @@ supporterFromString = splitFields >>> supporterFromFieldList
         -- other sizes are invalid, so return nothing
         supporterFromFieldList _ = Nothing
     
-loadSupporters :: IO ([Supporter])
-loadSupporters = getContents >>= ( -- process the results through a pure processing pipeline:
+loadSupporters :: FilePath -> IO ([Supporter])
+loadSupporters fn = 
+    readFile fn >>= ( -- read file and process the results through a pure processing pipeline:
                     lines >>>                    -- break the string's content into lines
                     fmap supporterFromString >>> -- convert each line into a Maybe Supporter
                     catMaybes >>>                -- remove the Maybe, discarding any Nothings
@@ -69,17 +72,45 @@ peopleToTubs people = ceiling (tubs)
     where
         scoops = (people * 3) % 2
         tubs = scoops / 9
-        
-main :: IO ()
-main = 
-    loadSupporters 
+
+formatReport :: String -> Map.Map Flavor Integer -> String
+formatReport scenario flavorCounts =
+    scenario ++ ":\n" ++ (
+        Map.foldrWithKey addLineForEntry "" flavorCounts)
+    where
+        addLineForEntry flavorName count existingLines = 
+            " * " ++ (show flavorName) ++ " " ++ (show count) ++ "\n" ++ existingLines
+
+produceScenarioReport :: FilePath -> String -> IO String
+produceScenarioReport filename scenario =
+    loadSupporters filename
         >>= ( -- once we've got a supporters list we need to process it through a pure pipeline, which we'll define
               -- here using arrows for convenient composition of functions:
               buildFlavorList >>>           -- first extract the flavors and format each supporter as a list ready for conversion to a map
                 Map.fromListWith (++) >>>   -- then convert to a map, concatenating the lists of supporters with the same flavor
                 Map.map (countPeople >>>    --   for each value in the map, count the number of people eating each flavor
                          peopleToTubs) >>>  --   and calculate how many tubs are needed
-                show >>> return             -- finally convert to a string, and then encapsulate the resulting string in an IO monad)
+                formatReport scenario >>>   -- finally convert to a string
+                return                      -- and then encapsulate the resulting string in an IO monad
             )
-        >>= putStrLn
 
+filePathForScenario :: Integer -> FilePath
+filePathForScenario n = "data" </> "challenge_20_s" ++ (show n) <.> "txt"
+
+-- produce names for the final report; there doesn't seem to be a neater way of doing it than this
+-- there is a package "numerals" that can produce it, but it would need more code to use it, so
+-- this is simpler:
+nameForScenario :: Integer -> String
+nameForScenario 1 = "Scenario one"
+nameForScenario 2 = "Scenario two"
+nameForScenario 3 = "Scenario three"
+nameForScenario n = "Scenario " ++ (show n) -- default fallback for unexpected numbers
+
+main :: IO ()
+main = do
+    -- print a report for each scenario, and ignore the collected results
+    _ <- forM 
+        [ (filePathForScenario n, nameForScenario n) | n <- [1..3] ] 
+        (\ (path, scenario) -> produceScenarioReport path scenario >>= putStrLn)
+    -- and return nothing
+    return ()
